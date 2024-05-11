bc1e53dfd282349fc7d1a1ecc0b6c450cd212a0e
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-05-11 22:02:37 +0600

    mongodb server connected with volunteerneed get request

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const express = require('express')
 const app = express()
 const cors = require("cors")
-const port = 3000
-
+require('dotenv').config()
+const port = process.env.PORT || 8000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jp6bbe4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    
+    const volunteerNeedsNow = client.db('VolunteerManagement').collection('VolunteerNeedsNow')
+    app.get('/volunteerneed', async(req,res)=>{
+      const cursor = volunteerNeedsNow.find()
+        const result = await cursor.toArray()
+        res.send(result)
+      }
+    )
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
