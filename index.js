847735ac16da1107618ef07c660b9acfd5cded85
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-05-14 18:53:41 +0600

    my post delte by DELETE Method

--- index.js
+++ index.js
@@ -118,6 +118,14 @@ app.put('/updatepost/:id',async(req,res)=>{
 })
 
 
+app.delete('/updatepost/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await addVolunteerData.deleteOne(query)
+      res.send(result)
+})
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
